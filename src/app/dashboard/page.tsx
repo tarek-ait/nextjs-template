f8358a38094b3e35f1e48e72a92fd51963a1ac85
Tarek Ait Ahmed <159696248+tarek-ait@users.noreply.github.com>
2025-04-11 21:19:48 +0200

    test commmit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  const name = 'John Doe';
+  const age = 30;
+  const hobbies = ['Reading', 'Traveling', 'Gaming'];
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is the dashboard page.</p>
+    </div>
+  );
+}
