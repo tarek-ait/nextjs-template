b58860b72f734b7fdf94495496ab81c1bd1ad1d4
Tarek Ait Ahmed <159696248+tarek-ait@users.noreply.github.com>
2025-04-12 13:25:48 +0200

    adding jest for tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import prisma from '../lib/prisma';
-
-export default async function Home() {
-  const users = await prisma.user.findMany();
+export default function Home() {
   return (
     <div className="-mt-16 flex min-h-screen flex-col items-center justify-center bg-gray-50">
       <h1 className="mb-8 font-[family-name:var(--font-geist-sans)] text-4xl font-bold text-[#333333]">
         Superblog
       </h1>
-      <ol className="list-inside list-decimal font-[family-name:var(--font-geist-sans)]">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2 text-lg text-gray-700">
-            {user.name}
-          </li>
-        ))}
-      </ol>
     </div>
   );
 }
